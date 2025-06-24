b573e51908c364ed963d59ab095aef78569ee805
kishima <kishima7@gmail.com>
2025-06-24 12:12:18 +0900

    dev snap shot

--- main/storage/main.rb
+++ main/storage/main.rb
@@ -3,8 +3,6 @@ puts "start I2C control"
 i2c = ESP32::I2C.new()
 i2c.init(ESP32::I2C::PORT0, {sda: 0, scl: 26, freq: 400000})
 
-puts "i2c ready"
-
 # 0x20 : RGB LED color
 # 0x50 : Joy Raw data
 # 0x60 : Stick X,Y value (4)
@@ -18,13 +16,25 @@ def i2c_read_8bit(m, reg_addr)
   result ? result.bytes[0] : 0
 end
 
+# init micro ROS
+
+# create Node
+
+# register publisher
+
+
 loop do
-    stick_left_x = i2c_read_8bit(i2c, 0x60)
-    stick_left_y = i2c_read_8bit(i2c, 0x61)
-    stick_right_x = i2c_read_8bit(i2c, 0x62)
-    stick_right_y = i2c_read_8bit(i2c, 0x63)
-    puts "#{stick_left_x},#{stick_left_y},#{stick_right_x},#{stick_right_y}"
-    ESP32::System.delay(100)
+  stick_left_x = i2c_read_8bit(i2c, 0x60)
+  stick_left_y = i2c_read_8bit(i2c, 0x61)
+  stick_right_x = i2c_read_8bit(i2c, 0x62)
+  stick_right_y = i2c_read_8bit(i2c, 0x63)
+  puts "#{stick_left_x},#{stick_left_y},#{stick_right_x},#{stick_right_y}"
+
+  #make joy topic
+  #publish topic
+
+  #spin
+  ESP32::System.delay(100)
 end
 
 i2c.deinit
